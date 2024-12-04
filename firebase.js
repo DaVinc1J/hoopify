d7008e75a03db7224db112d2f09f08542e76a440
davincij <tristanlowe48@gmail.com>
2024-12-04 16:55:48 +1030

    changing to realtime data base

--- firebase.js
+++ firebase.js
@@ -1,26 +1,30 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push } from "firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.16.0/firebase-app.js";
+import {
+	getDatabase,
+	ref,
+	push,
+} from "https://www.gstatic.com/firebasejs/9.16.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 export const submitFormData = async (formData) => {
-  try {
-    const dataRef = ref(database, "formEntries");
-    await push(dataRef, formData);
-    alert("Form submitted successfully!");
-  } catch (error) {
-    console.error("Error submitting form:", error);
-    alert("Failed to submit form. Please try again later.");
-  }
+	try {
+		const dataRef = ref(database, "formEntries");
+		await push(dataRef, formData);
+		alert("Form submitted successfully!");
+	} catch (error) {
+		console.error("Error submitting form data:", error);
+		alert("Failed to submit form. Please try again later.");
+	}
 };
